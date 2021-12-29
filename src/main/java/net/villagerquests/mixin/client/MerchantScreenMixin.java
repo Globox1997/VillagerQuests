3ffa3b8958d0bc623f6e4dccc035c96f17e8e6ec
Globox1997 <mitschke11@web.de>
2021-12-29 12:40:33 +0100

    added unclickable button

--- src/main/java/net/villagerquests/mixin/client/MerchantScreenMixin.java
+++ src/main/java/net/villagerquests/mixin/client/MerchantScreenMixin.java
@@ -1,5 +1,9 @@
 package net.villagerquests.mixin.client;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import com.mojang.blaze3d.systems.RenderSystem;
 
 import org.spongepowered.asm.mixin.Mixin;
@@ -17,6 +21,7 @@ import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.screen.MerchantScreenHandler;
 import net.minecraft.text.Text;
 import net.villagerquests.accessor.MerchantAccessor;
+import net.villagerquests.accessor.PlayerAccessor;
 import net.villagerquests.gui.QuestScreenHandler;
 import net.villagerquests.network.QuestClientPacket;
 
@@ -24,8 +29,29 @@ import net.villagerquests.network.QuestClientPacket;
 @Mixin(MerchantScreen.class)
 public abstract class MerchantScreenMixin extends HandledScreen<MerchantScreenHandler> implements MerchantAccessor {
 
+    private final List<Integer> questIdList = new ArrayList<>();
+
     public MerchantScreenMixin(MerchantScreenHandler handler, PlayerInventory inventory, Text title) {
         super(handler, inventory, title);
+    }
+
+    @Inject(method = "init", at = @At(value = "TAIL"))
+    protected void initMixin(CallbackInfo info) {
+        this.questIdList.clear();
+        this.questIdList.addAll(((MerchantAccessor) ((MerchantAccessor) this.client.player).getCurrentOfferer()).getQuestIdList());
+
+        Iterator<Integer> iterator = this.questIdList.iterator();
+        while (iterator.hasNext()) {
+            int check = iterator.next();
+            int questId = this.questIdList.get(this.questIdList.indexOf(check));
+            if ((((PlayerAccessor) this.client.player).getPlayerQuestIdList().contains(questId)
+                    && !((PlayerAccessor) this.client.player).isOriginalQuestGiver(((MerchantAccessor) this.client.player).getCurrentOfferer().getUuid(), questId))
+                    || (((PlayerAccessor) this.client.player).getPlayerFinishedQuestIdList().contains(questId) && ((PlayerAccessor) this.client.player).getPlayerQuestRefreshTimerList()
+                            .get(((PlayerAccessor) this.client.player).getPlayerFinishedQuestIdList().indexOf(questId)).equals(-1))) {
+                iterator.remove();
+            }
+
+        }
     }
 
     @Inject(method = "drawBackground", at = @At(value = "TAIL"))
@@ -34,16 +60,19 @@ public abstract class MerchantScreenMixin extends HandledScreen<MerchantScreenHa
         int j = (this.height - this.backgroundHeight) / 2;
 
         RenderSystem.setShaderTexture(0, QuestScreenHandler.GUI_ICONS);
-        if (this.isPointWithinBounds(276, 0, 20, 20, (double) mouseX, (double) mouseY)) {
-            MerchantScreenMixin.drawTexture(matrices, i + 276, j, 20, 0, 20, 20, 512, 512);
-        } else
-            MerchantScreenMixin.drawTexture(matrices, i + 276, j, 0, 0, 20, 20, 512, 512);
+        if (!questIdList.isEmpty())
+            if (this.isPointWithinBounds(276, 0, 20, 20, (double) mouseX, (double) mouseY))
+                MerchantScreenMixin.drawTexture(matrices, i + 276, j, 20, 0, 20, 20, 512, 512);
+            else
+                MerchantScreenMixin.drawTexture(matrices, i + 276, j, 0, 0, 20, 20, 512, 512);
+        else
+            MerchantScreenMixin.drawTexture(matrices, i + 276, j, 80, 0, 20, 20, 512, 512);
     }
 
     // In window: 251 and 5
     @Inject(method = "mouseClicked", at = @At(value = "HEAD"), cancellable = true)
     private void mouseClickedMixin(double mouseX, double mouseY, int button, CallbackInfoReturnable<Boolean> info) {
-        if (this.isPointWithinBounds(276, 0, 20, 20, (double) mouseX, (double) mouseY)) {
+        if (this.isPointWithinBounds(276, 0, 20, 20, (double) mouseX, (double) mouseY) && !questIdList.isEmpty()) {
             QuestClientPacket.writeC2SScreenPacket(((MerchantAccessor) this.client.player).getCurrentOfferer(), (int) this.client.mouse.getX(), (int) this.client.mouse.getY());
             info.cancel();
         }
