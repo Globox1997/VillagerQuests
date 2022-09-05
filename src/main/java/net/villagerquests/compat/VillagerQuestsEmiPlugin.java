0d0828f0b019a6e4ef35cea29f223f0696db3864
Globox1997 <mitschke11@web.de>
2022-09-05 14:55:50 +0200

    fixed emi exclusion

--- src/main/java/net/villagerquests/compat/VillagerQuestsEmiPlugin.java
+++ src/main/java/net/villagerquests/compat/VillagerQuestsEmiPlugin.java
@@ -3,14 +3,14 @@ package net.villagerquests.compat;
 import dev.emi.emi.api.EmiPlugin;
 import dev.emi.emi.api.EmiRegistry;
 import dev.emi.emi.api.widget.Bounds;
-import net.minecraft.client.gui.screen.ingame.InventoryScreen;
+import net.minecraft.client.gui.screen.ingame.MerchantScreen;
 import net.villagerquests.VillagerQuestsMain;
 
 public class VillagerQuestsEmiPlugin implements EmiPlugin {
 
     @Override
     public void register(EmiRegistry registry) {
-        registry.addExclusionArea(InventoryScreen.class, (screen, consumer) -> {
+        registry.addExclusionArea(MerchantScreen.class, (screen, consumer) -> {
             if (VillagerQuestsMain.CONFIG.exclusionZone) {
                 int i = (screen.width - 276) / 2;
                 int j = (screen.height - 166) / 2;
